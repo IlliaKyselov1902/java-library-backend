1d9eaed389cfe6e7f4c7d89dcdfb0e5e779d4475
Illia Kyselov <qwertytwenty129@gmail.com>
2023-04-10 23:46:45 +0300

    Finished work with entities

--- src/main/java/com/example/library/model/entity/Author.java
+++ src/main/java/com/example/library/model/entity/Author.java
@@ -2,6 +2,7 @@ package com.example.library.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,12 +19,14 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String surname;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate birthday;
     @ElementCollection(targetClass = Genre.class)
-    private Set<Genre> skillSet;
+    private Set<Genre> genreSet;
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     private List<Book> proofs;
 }
